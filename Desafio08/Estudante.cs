ea52f01304b95718d54a84d9edacd66c2d8fc568
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 16:29:50 -0300

    Desafio 08

---
+++ Desafio08/Estudante.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio08
+{
+    public class Estudante
+    {
+        public string Nome { get; private set; }
+
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+
+        // Propriedade somente leitura que calcula a média
+        public double Media
+        {
+            get { return (Nota1 + Nota2) / 2; }
+        }
+
+        // Propriedade somente leitura que calcula a situação
+        public string Situacao
+        {
+            get { return Media >= 6 ? "Aprovado" : "Reprovado"; }
+        }
+
+        public Estudante(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void ExibirBoletim()
+        {
+            Console.WriteLine($"Estudante: {Nome}");
+            Console.WriteLine($"Média: {Media.ToString("N2", new CultureInfo("pt-BR"))}");
+            Console.WriteLine($"Situação: {Situacao}");
+        }
+    }
+}
---
+++ Desafio08/Program.cs
@@ -0,0 +1,9 @@
+﻿using Desafio08;
+
+Estudante estudante = new Estudante("Lucas Rocha");
+estudante.Nota1 = 7.5;
+estudante.Nota2 = 5.0;
+
+estudante.ExibirBoletim();
+
+Console.ReadKey();
