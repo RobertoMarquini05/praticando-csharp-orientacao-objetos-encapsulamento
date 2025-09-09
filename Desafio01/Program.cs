4caf15f543b6f6171d46e3b18b2164d1e2a904b4
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 16:25:09 -0300

    Desafio 0

---
+++ Desafio01/Program.cs
@@ -0,0 +1,6 @@
+﻿using praticando_csharp_orientacao_objetos_encapsulamento;
+
+Veiculo veiculo = new Veiculo("ABC-1234");
+veiculo.AtualizarVelocidade(72.5);
+
+Console.ReadKey();
---
+++ Desafio01/Veiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praticando_csharp_orientacao_objetos_encapsulamento
+{
+    public class Veiculo
+    {
+        public Guid Id { get; set; }
+        public string Placa {  get; set; }
+        public double VelocidadeAtual {  get; private set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Veiculo(string placa) 
+        {
+            Id = Guid.NewGuid();
+            Placa = placa;
+            VelocidadeAtual = 0;
+            CreatedAt = DateTime.Now;
+        }    
+
+        public void AtualizarVelocidade(double novaVelocidade)
+        {
+            VelocidadeAtual = novaVelocidade;
+
+            Console.WriteLine($"Veículo: {Placa}\nVelocidade Atual: {VelocidadeAtual}km/h");
+        }
+    }
+}
