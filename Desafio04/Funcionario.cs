c921656d98c28df53ba8ee993cef476f757494e9
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 16:26:43 -0300

    Desafio 04

---
+++ Desafio04/Funcionario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04
+{
+    public class Funcionario
+    {
+        public string Nome { get; private set; }
+
+        private double salario;
+
+        // Propriedade só de leitura
+        public double Salario
+        {
+            get { return salario; }
+        }
+
+        public Funcionario(string nome, double salarioInicial)
+        {
+            Nome = nome;
+            salario = salarioInicial;
+        }
+
+        public void ReajustarSalario(double novoValor)
+        {
+            if (novoValor <= salario)
+            {
+                Console.WriteLine("Erro: O novo salário deve ser maior que o atual.");
+            }
+            else
+            {
+                salario = novoValor;
+            }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Funcionário: {Nome}");
+            Console.WriteLine($"Salário atual: {Salario.ToString("C", new CultureInfo("pt-BR"))}");
+        }
+    }
+}
---
+++ Desafio04/Program.cs
@@ -0,0 +1,10 @@
+﻿using Desafio04;
+
+Funcionario f = new Funcionario("Fernanda Lima", 4000);
+
+f.ReajustarSalario(3500); // Inválido
+f.ReajustarSalario(4200); // Válido
+
+f.ExibirDados();
+
+Console.ReadKey();
