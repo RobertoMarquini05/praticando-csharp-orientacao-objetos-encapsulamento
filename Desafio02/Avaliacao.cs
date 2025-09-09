063479d98523c1888f19b150b8095680b3d4aafe
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 16:25:44 -0300

    Desafio02

---
+++ Desafio02/Avaliacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02
+{
+    public class Avaliacao
+    {
+        public string Aluno { get; private set; }
+
+        // Nota só pode ser lida fora da classe
+        public double Nota { get; private set; }
+
+        public Avaliacao(string aluno)
+        {
+            Aluno = aluno;
+        }
+
+        public void AtribuirNota(double nota)
+        {
+            if (nota < 0 || nota > 10)
+            {
+                Console.WriteLine("Erro: A nota deve estar entre 0 e 10.");
+            }
+            else
+            {
+                Nota = nota;
+            }
+        }
+
+        public void ExibirResultado()
+        {
+            Console.WriteLine($"Aluno: {Aluno}");
+            Console.WriteLine($"Nota atribuída: {Nota}");
+        }
+    }
+}
---
+++ Desafio02/Program.cs
@@ -0,0 +1,10 @@
+﻿using Desafio02;
+
+Avaliacao avaliacao = new Avaliacao("Carla Silva");
+
+avaliacao.AtribuirNota(11);   
+avaliacao.AtribuirNota(8.5);  
+
+avaliacao.ExibirResultado();
+
+Console.ReadKey();
