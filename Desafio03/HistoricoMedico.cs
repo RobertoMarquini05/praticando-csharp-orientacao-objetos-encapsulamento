5c8100a44b339ab6068a87119084d4fa95691a2b
Roberto Marquini <marquini.roberto05@gmail.com>
2025-09-09 16:26:14 -0300

    Desafio03

---
+++ Desafio03/HistoricoMedico.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio03
+{
+    internal class HistoricoMedico
+    {
+        public string CodigoProntuario { get; private set; }
+
+        public HistoricoMedico(string codigoProntuario)
+        {
+            CodigoProntuario = codigoProntuario;
+        }
+
+        public void ExibirCodigo()
+        {
+            Console.WriteLine($"Código do prontuário: {CodigoProntuario}");
+        }
+    }
+}
---
+++ Desafio03/Program.cs
@@ -0,0 +1,9 @@
+﻿using Desafio03;
+
+Paciente paciente = new Paciente("Luiz Costa", 42);
+HistoricoMedico historico = new HistoricoMedico("XPT-9987");
+
+Console.WriteLine($"Paciente: {paciente.Nome}, Idade: {paciente.Idade}");
+historico.ExibirCodigo();
+
+Console.ReadKey();
